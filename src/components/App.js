f3ddbc2c91ac8f197eb0fc9655ea42e570e71323
Ryan Filho <ryanfilhoryanfilho@gmail.com>
2018-03-25 14:32:36 -0300

    Change serve url

--- src/components/App.js
+++ src/components/App.js
@@ -42,8 +42,8 @@ class App extends Component {
 					   body: JSON.stringify({ code: this.state.code }),
 					   
 					};	
-		// fetch("https://us-central1-vbjs-6639a.cloudfunctions.net/lexAnalyser", myInit)
-		fetch("http://localhost:5000/vbjs-6639a/us-central1/lexAnalyser", myInit)		
+		fetch("https://us-central1-vbjs-6639a.cloudfunctions.net/lexAnalyser", myInit)
+		// fetch("http://localhost:5000/vbjs-6639a/us-central1/lexAnalyser", myInit)		
 		.then(this.handleErrors)
 		.then(data =>	data.json())
 		.then(this.updateGrids)
